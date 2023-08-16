4c51c3ec4fe8886ea7d1f287d50c2fd94ebd225d
Pierre Caillaud Menéndez <pcaillaudm@gmail.com>
2023-08-16 20:32:27 +0200

    setup github actions

---
+++ svelte.config.js
@@ -0,0 +1,11 @@
+import adapter from "@sveltejs/adapter-static"
+
+export default {
+  kit: {
+    adapter: adapter({
+      pages: "docs",
+      assets: "docs",
+      fallback: "index.html",
+    }),
+  },
+}
