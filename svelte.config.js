b87bc8a4cc8e0beeebc6e5e696206d569e0c49f3
Pierre Caillaud Menéndez <pcaillaudm@gmail.com>
2023-08-16 22:23:53 +0200

    fix deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,15 @@
 import adapter from "@sveltejs/adapter-static"
 
+const dev = process.argv.includes("dev")
+
+/** @type {import('@sveltejs/kit').Config} */
 export default {
   kit: {
     adapter: adapter({
-      pages: "docs",
-      assets: "docs",
       fallback: "index.html",
     }),
     paths: {
-      base: "/brunify",
+      base: dev ? "" : process.env.BASE_PATH,
     },
   },
 }
